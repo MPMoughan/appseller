90e79a3aba22637047ddbec7619b5309db1dc480
bamiano <bamiano@email.arizona.edu>
2014-12-17 11:02:37 -0800

    API call and manufacturer search added

--- app/assets/javascripts/apisetup.js
+++ app/assets/javascripts/apisetup.js
@@ -1,28 +1,46 @@
 // var apiCall = function(searchTerm, callback){
+var dataSource;
+var dataShowTemplate;
+
+$(document).ready(function(){
+  dataSource = $("#get-data").html();
+  dataShowTemplate = Handlebars.compile(dataSource)
+});
+
+
+
+
 
 $(document).on("click", "#additem", function(){
-  var url = "http://api.remix.bestbuy.com/v1/products(manufacturer=samsung&salePrice%3E100)?format=json&show=name,salePrice,image&apiKey=t72cd4zb4ahwvx49ad3xqasu"
+  var brand = $("#brand").val();
+  var url = "http://api.remix.bestbuy.com/v1/products(manufacturer=" + brand + "&salePrice%3E400)?format=json&show=name,salePrice,image&apiKey=t72cd4zb4ahwvx49ad3xqasu";
   // "http://api.remix.bestbuy.com/v1/products";
   // url += "(manufacturer=samsung&salePrice%3E100)";
   // url += "?format=json&show=name,salePrice,image&apiKey=t72cd4zb4ahwvx49ad3xqasu";
+
   $.ajax({
     url: url,
     type: "GET",
     dataType: "jsonp",
     cache: true,
     success: function(data) {
-      console.log("data", data);
-      $.each(data.products.name);
+      // test to see what we are retrieving from the API
+      // console.log("data", data);
+      // testing if we can go a layer deeper with the API response
+      // console.log(data.products);
+      // put this data into an object and use handlebars to render it
 
-      // callback(data);
-      // var html = productShowTemplate({productData: data});
-      // $("#content").html(html);
+      viewData = dataShowTemplate({Data: data.products});
+      $("#content").html(viewData);
+      console.log(data.products[0]);
     },
     error: function() {
       alert("Something went wrong here...");
     }
   });
 });
+
+
 
 
 
